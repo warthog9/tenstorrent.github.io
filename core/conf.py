3fce42895454b6158df087029e79e13d07dceb93
Dimitri Gnidash <119051828+dimitri-tenstorrent@users.noreply.github.com>
2024-06-11 13:24:54 -0400

    #35: Remove the version dropdown from core (#46)

--- core/conf.py
+++ core/conf.py
@@ -28,6 +28,7 @@ html_favicon = "../shared/images/cropped-favicon-32x32.png"
 html_static_path = ['../shared/_static']
 
 html_context = {
+    "versions": None,
     "logo_link_url": "https://tenstorrent.github.io/docs-test/core/latest/"
 }
 
