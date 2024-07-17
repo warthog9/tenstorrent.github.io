dce776ee6f9d04ad6fa19872e4ed3f7f68961c5c
Dimitri Gnidash <dimitri@tenstorrent.com>
2024-07-17 14:57:44 -0400

    Fix the missing import

--- core/conf.py
+++ core/conf.py
@@ -5,6 +5,7 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import os
 
 project = 'Tenstorrent'
 copyright = '2024, Tenstorrent'
