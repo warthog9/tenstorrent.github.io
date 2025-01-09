50120a25f6c054b28b46a08a04ba51830a650885
Dimitri Gnidash <dimitri@tenstorrent.com>
2025-01-09 14:52:31 -0500

    Prepare for v0.54 release of ttnn and tt-metalium

--- update_tags.py
+++ update_tags.py
@@ -11,10 +11,13 @@ version_structure = {version}
 
 if project in ["ttnn", "tt-metalium"]:
     version_no_v = version.replace("v", "")
+    #
+    # From version 0.54 forward, the wheel files were renamed to ttnn from metal_libs.
+    #
     additional_cmd = \
-        f"wget https://github.com/tenstorrent/tt-metal/releases/download/{version}/metal_libs-{version_no_v}+wormhole.b0-cp38-cp38-linux_x86_64.whl\n"
+        f"wget https://github.com/tenstorrent/tt-metal/releases/download/{version}/ttnn-{version_no_v}+wormhole.b0-cp38-cp38-linux_x86_64.whl\n"
     additional_cmd += \
-        f"pip install --extra-index-url https://download.pytorch.org/whl/cpu metal_libs-{version_no_v}+wormhole.b0-cp38-cp38-linux_x86_64.whl --force-reinstall"
+        f"pip install --extra-index-url https://download.pytorch.org/whl/cpu ttnn-{version_no_v}+wormhole.b0-cp38-cp38-linux_x86_64.whl --force-reinstall"
     version_structure = {version: {"additional_cmd": additional_cmd}}
 
 versions[project]["versions"].update(version_structure)
